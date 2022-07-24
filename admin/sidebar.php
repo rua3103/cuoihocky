fa0e7daf70ba3c1e1f309e230d35a5db6fffca8f
rua3103 <94063033+rua3103@users.noreply.github.com>
2022-07-24 13:22:31 +0700

    a

---
+++ admin/sidebar.php
@@ -0,0 +1,45 @@
+
+
+<!-- MENU SIDEBAR-->
+<aside class="menu-sidebar d-none d-lg-block">
+    <div class="logo">
+        <a href="#">
+            <img src="images/icon/logo.png" alt="Cool Admin" />
+        </a>
+    </div>
+    <div class="menu-sidebar__content js-scrollbar1">
+        <nav class="navbar-sidebar">
+            <ul class="list-unstyled navbar__list">
+                <li class="active has-sub">
+                    <a class="js-arrow" href="index.php">
+                        <i class="fas fa-tachometer-alt"></i>Home</a>
+                </li>
+                <li>
+                    <a href="blog_category.php">
+                        <i class="fas fa-chart-bar"></i>Blog Categories</a>
+                </li>
+                <li>
+                    <a href="blogs.php">
+                        <i class="fas fa-table"></i>Blogs</a>
+                </li>
+                <li>
+                    <a href="write_a_blog.php">
+                        <i class="fas fa-edit"></i>Write A Blog</a>
+                </li>
+                <li>
+                    <a href="blog_contact.php">
+                        <i class="far fa-check-square"></i>Blog Contacts</a>
+                </li>
+                <li>
+                    <a href="blog_subscriber.php">
+                        <i class="fas fa-calendar-alt"></i>Blog Subscribers</a>
+                </li>
+                <li>
+                    <a href="blog_comment.php">
+                        <i class="fas fa-map-marker-alt"></i>Blog Comments</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
+<!-- END MENU SIDEBAR-->
